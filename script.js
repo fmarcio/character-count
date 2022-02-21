fda5c465b9f2ea52924a219ae46ea5e92fd72697
Marcio Fonseca <marcio.fonseca@liferay.com>
2022-02-20 21:29:29 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById("submit-btn");
+const charInput = document.getElementById("char");
+const wordInput = document.getElementById("word");
+const spanWord = document.querySelector(".span-word");
+const spanChar = document.querySelector(".span-char");
+const spanCount = document.querySelector(".span-count");
+
+const countChar = (string, char) => {
+  string = wordInput.value.toLowerCase();
+  char = charInput.value.toLowerCase();
+
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) count++;
+  }
+
+  spanWord.innerText = string;
+  spanChar.innerText = char;
+  spanCount.innerText = count;
+};
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  countChar();
+});
